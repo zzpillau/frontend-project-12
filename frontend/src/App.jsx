fe728d2e4bedecbf850c92a1b7f87d60cb374e89
zzpillau <zzpillau@gmail.com>
2025-06-09 10:37:33 +0200

    chore: install bootstrap

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,15 +2,16 @@
 import React from 'react'
 
 import reactLogo from './assets/react.svg'
-import './App.css'
+import './scss/App.scss'
 
 const App = () => {
   return (
     <>
       <img src={reactLogo} alt="reactLogo" />
       <h1>
-        Hi!
+        Hi! I'm React Component
       </h1>
+      <button className='btn btn-primary'>I'm Bootstrap Button</button>
     </>
   )
 }
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,7 +2,8 @@ import React from 'react'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 
-import './index.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import './scss/index.scss'
 
 import App from './App.jsx'
 
